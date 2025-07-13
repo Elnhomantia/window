c7d11f2bc860889e9e859127e2b867eda4332ad8
Ithier Débauché <ithier.debauche@etu.univ-lyon1.fr>
2025-07-13 07:33:08 +0200

    Documentation

--- include/autoWindow.h
+++ include/autoWindow.h
@@ -6,27 +6,42 @@
 template<WindowConcept T>
 using CheckedWindow = T;
 
+/**
+ * @typedef AutoWindow
+ * @brief Static abstraction over an OS specific implementation that verify the @ref WindowConcept.
+ * It chooses based on your compilation plateform OS.
+ */
+
 #if defined(_WIN32)
     #include <Win32/Win32_window.h>
     using AutoWindow = CheckedWindow<Win32_window>;
-#elif defined(__linux__)
+#endif
+#if defined(__linux__)
     #error "Unsupported platform for now"
-#else
-    #error "Unsupported platform"
 #endif
 
+/**
+ * @brief Factory to abstact creation of a polymorphic window.
+ */
 class WindowFactory
 {
 public:
+    /**
+     * @brief Get a polymorphic instance of a window.
+     * 
+     * @param title Title of the window
+     * @param width Width of the window
+     * @param height Height of the window
+     * @return Window* A pointer to a new instance of an OS specific implementation of window.
+     */
     static Window* getInstance(const char* title, const unsigned int width, const unsigned int height)
     {
         #if defined(_WIN32)
             #include <Win32/Win32_window_polymorph.h>
             return new Win32_window_polymorph(title, width, height);
-        #elif defined(__linux__)
+        #endif
+        #if defined(__linux__)
             #error "Unsupported platform for now"
-        #else
-            #error "Unsupported platform"
         #endif
     }
 };
--- include/window.h
+++ include/window.h
@@ -6,6 +6,12 @@
 #include <sharedAbstration.h>
 #include <vulkan/vulkan.h>
 
+/**
+ * @brief Interface contract. Any implementation should verify this concept.
+ * List all needed functions and verify inherance with @ref SharedAbstraction.
+ * 
+ * @tparam T Implementation class.
+ */
 template<typename T>
 concept WindowConcept = 
     std::derived_from<T, SharedAbstraction> &&
@@ -34,31 +40,117 @@ concept WindowConcept =
         {WindowManagerConst.createVulkanSurface(instance)} -> std::convertible_to<VkSurfaceKHR>;
 };
 
+/**
+ * @brief Window polymorphic interface.
+ * A window is not thread safe and should always be ran in the same thread it was created.
+ * You should never need to downcast this.
+ */
 class Window : virtual public SharedAbstraction
 {
 public:
 
     virtual ~Window() = default;
 
+    /**
+     * @brief Set the Window visibility flag. 
+     * 
+     * @param flag See @ref WindowFlag.
+     */
     virtual void setWindowFlag(WindowFlag flag) = 0;
+    /**
+     * @brief Event loop handler. This does not update the content of the window.
+     */
     virtual void update() = 0;
+    /**
+     * @brief Force pixel buffer update.
+     */
     virtual void render() = 0;
+    /**
+     * @brief basic event loop for a window. 
+     * This will lock its thread whil the window is running.
+     * Don't call in your main thread. The window should be created where you call @ref exec.
+     */
     virtual void exec() = 0;
+    /**
+     * @brief Close the window definitively.
+     */
     virtual void close() = 0;
+    /**
+     * @brief Get if the window is still running.
+     * 
+     * @return true The window is running.
+     * @return false The window is not running.
+     */
     virtual bool isRunning() const = 0;
+    /**
+     * @brief Set the Dimentions object.
+     * 
+     * @param dimentions The new Dimentions object.
+     */
     virtual void setDimentions(const WindowDimentions & dimentions) = 0;
+    /**
+     * @brief Get the Dimentions object.
+     * 
+     * @return Dimentions object of the window.
+     */
     virtual const WindowDimentions& getDimentions() const = 0;
 
+    /**
+     * @brief Mouses events are emited only if called.
+     * See @ref MouseEvent @ref SharedAbstraction::resizeEvent for event structure and calls.
+     * 
+     * @return true Success
+     * @return false Failure
+     */
     virtual bool registerMouse() = 0;
+    /**
+     * @brief Keyboards events are emited only if called.
+     * See @ref KeyEvent @ref SharedAbstraction::resizeEvent for event structure and calls.
+     * 
+     * @return true Success.
+     * @return false Failure.
+     */
     virtual bool registerKeyboard() = 0;
 
+    /**
+     * @brief Change window dimention to occupy all the current screen.
+     */
     virtual void setFullscreen() = 0;
+    /**
+     * @brief Change window to borderless.
+     * 
+     * @param borderless true to borderless, false for "normal".
+     */
     virtual void setBorderless(const bool borderless) = 0;
+    /**
+     * @brief Enable or disable user resize of the window.
+     * Only affect users interractions. 
+     * @bug You should probably set resizable to false too, else a white line stay as handle on Windows.
+     * 
+     * @param resizable true to be able to resize, false to lock.
+     */
     virtual void setResizable(bool resizable) = 0;
 
+    /**
+     * @brief Hide or show cursor.
+     * 
+     * @param show true to show, false to hide.
+     */
     virtual void showCursor(bool show) = 0;
+    /**
+     * @brief Clip or free cursor from the window. 
+     * If clipped the cursor won't be able to go ouside the window when the focus is on the window.
+     * 
+     * @param clip true to clip, false to free.
+     */
     virtual void clipCursor(bool clip) = 0;
 
+    /**
+     * @brief Create a Vulkan Surface object.
+     * 
+     * @param instance A vulkan instance.
+     * @return VkSurfaceKHR A vulkan surface.
+     */
     virtual VkSurfaceKHR createVulkanSurface(VkInstance instance) const = 0;
 };
 
